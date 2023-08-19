9523e4ee7144e8a615aa503305766a38fdf21080
Benjamin Hamon <development@benjaminhamon.com>
2023-08-19 21:20:54 +0200

    Add project base

---
+++ Sources/website/setup.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+import setuptools
+
+workspace_directory = os.path.abspath(os.path.join("..", ".."))
+automation_setup_directory = os.path.join(workspace_directory, "Automation", "Setup")
+
+sys.path.insert(0, automation_setup_directory)
+
+import automation_helpers # pylint: disable = wrong-import-position
+
+
+def run_setup() -> None:
+    project_configuration = automation_helpers.load_project_configuration(workspace_directory)
+
+    setuptools.setup(
+		name = "benjaminhamon-writing-website",
+		description = "Website for www.benjaminhamon.com",
+        version = project_configuration["ProjectVersionFull"],
+        author = project_configuration["Author"],
+        author_email = project_configuration["AuthorEmail"],
+        url = project_configuration["ProjectUrl"],
+        packages = setuptools.find_packages(include = [ "benjaminhamon_writing_website", "benjaminhamon_writing_website.*" ]),
+        python_requires = "~= 3.9",
+    )
+
+
+run_setup()
